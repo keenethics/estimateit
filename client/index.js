993d79efd3e04f33d39bd0d950d7c216ea7523cb
sasha.bashliy <sashkacrab@gmail.com>
2017-03-20 12:21:33 +0200

    fixed

--- client/index.js
+++ client/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
-    <Route exact path="/" component={App} />
-  </Router>,
+  <App />,
   document.getElementById('root'));
