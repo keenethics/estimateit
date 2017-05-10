6ac9a2b2a872a44091ed32af2ef9590484371a3f
Delphit <m.mykhailyshyn@yandex.ru>
2017-05-10 21:34:49 +0300

    Added Redux State file

---
+++ client/store/index.js
@@ -0,0 +1,23 @@
+import { createStore, compose } from 'redux';
+import rootReducer from '../reducers';
+
+function configureStoreProd(initialState) {
+  return createStore(rootReducer, initialState);
+}
+
+function configureStoreDev(initialState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(rootReducer, initialState);
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers').default; // eslint-disable-line global-require
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
+}
+
+const configureStore = process.env.NODE_ENV === 'production' ? configureStoreProd : configureStoreDev;
+
+export default configureStore;
