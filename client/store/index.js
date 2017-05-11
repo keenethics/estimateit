2308289e8ad6ebb2744d1fabd8b32305ad57f436
Delphit <m.mykhailyshyn@yandex.ru>
2017-05-11 13:28:01 +0300

    Fix Redux Chrome DevTools

--- client/store/index.js
+++ client/store/index.js
@@ -7,7 +7,7 @@ function configureStoreProd(initialState) {
 
 function configureStoreDev(initialState) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState, composeEnhancers());
   if (module.hot) {
     module.hot.accept('../reducers', () => {
       const nextReducer = require('../reducers').default; // eslint-disable-line global-require
