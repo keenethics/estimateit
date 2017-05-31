d122a326d579e3b5a09a318ccd7479d29ed2f9f5
kuksik <nazarkukarkin@gmail.co>
2017-05-31 16:06:17 +0300

    added props validarion in Calculation.jsx

--- client/components/Estimator/Cards/Calculation/index.jsx
+++ client/components/Estimator/Cards/Calculation/index.jsx
@@ -1,35 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Card, CardBlock, Col } from 'reactstrap';
 import EstimateOptions from '../EstimateOptions';
 import PieChart from '../PieChart';
 import styles from './styles.scss';
 
-export default class Calculation extends Component {
-  render() {
-    return (
-      <Card className={styles.calculation}>
-        <CardBlock className={styles.calculation__wrapper}>
-          <Col
-            xs="12" lg="4"
-            className={styles.calculation__piechart}
-          >
-            <PieChart data={this.props.estimateOptions} />
-          </Col>
-          <Col
-            xs="12" lg="8"
-            className={styles.calculation__options}
-          >
-            <EstimateOptions
-              rate={this.props.rate}
-              hours={this.props.hours}
-              onRateChange={this.props.onRateChange}
-              estimateOptions={this.props.estimateOptions}
-              addEstimateOptions={this.props.addEstimateOptions}
-            />
-          </Col>
-        </CardBlock>
-      </Card>
-    );
-  }
+const Calculation = ({
+  rate,
+  hours,
+  onRateChange,
+  estimateOptions,
+  addEstimateOptions,
+}) => (
+  <Card className={styles.calculation}>
+    <CardBlock className={styles.calculation__wrapper}>
+      <Col
+        xs="12" lg="4"
+        className={styles.calculation__piechart}
+      >
+        <PieChart data={estimateOptions} />
+      </Col>
+      <Col
+        xs="12" lg="8"
+        className={styles.calculation__options}
+      >
+        <EstimateOptions
+          rate={rate}
+          hours={hours}
+          onRateChange={onRateChange}
+          estimateOptions={estimateOptions}
+          addEstimateOptions={addEstimateOptions}
+        />
+      </Col>
+    </CardBlock>
+  </Card>
+);
 
-}
+Calculation.propTypes = {
+  rate: PropTypes.number.isRequired,
+  hours: PropTypes.number.isRequired,
+  onRateChange: PropTypes.func.isRequired,
+  estimateOptions: PropTypes.object.isRequired,
+  addEstimateOptions: PropTypes.func.isRequired,
+};
+
+export default Calculation;
