88b199823681ccf9dbd36cd6c13f7369a02e6fdc
kuksik <nazarkukarkin@gmail.co>
2017-07-20 17:21:41 +0300

    fixed scroll

--- src/components/libs/scroll.js
+++ src/components/libs/scroll.js
@@ -1,19 +1,10 @@
 /* eslint-disable */
 const scrollToItem = function scrollToItem(errors) {
-  const fieldName = Object.keys(errors)[0];
-  const firstInvalidItem = document.getElementsByName(fieldName)[0];
-
-  if (!firstInvalidItem) return null;
-
-  const diff = (firstInvalidItem.offsetTop - window.scrollY) / 8;
-
-  if (Math.abs(diff) > 1) {
-    window.scrollTo(0, (window.scrollY + diff));
-    clearTimeout(window._TO);
-    window._TO = setTimeout(scrollToItem(errors), 30, firstInvalidItem);
-  } else {
-    window.scrollTo(0, firstInvalidItem.offsetTop);
-  }
+  setTimeout(() => {
+    const firstInvalidItem = document.getElementsByClassName('text-danger')[0];
+    if (!firstInvalidItem) return null;
+    firstInvalidItem.scrollIntoViewIfNeeded();
+ }, 0);
 };
 
 export default scrollToItem;
