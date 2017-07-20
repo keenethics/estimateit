d6e315f88e10e5c6a29ee102f5d1f868a8376bc7
kuksik <nazarkukarkin@gmail.co>
2017-07-20 19:26:14 +0300

    implemented crossbrowser scrolling

--- src/components/libs/scroll.js
+++ src/components/libs/scroll.js
@@ -1,9 +1,13 @@
+import scrollIntoViewIfNeeded from 'scroll-into-view-if-needed'
+
 /* eslint-disable */
 const scrollToItem = function scrollToItem(errors) {
   setTimeout(() => {
     const firstInvalidItem = document.getElementsByClassName('text-danger')[0];
     if (!firstInvalidItem) return null;
-    firstInvalidItem.scrollIntoViewIfNeeded();    
+    scrollIntoViewIfNeeded(firstInvalidItem, true, {
+      duration: 150
+    })
  }, 0);
 };
 
