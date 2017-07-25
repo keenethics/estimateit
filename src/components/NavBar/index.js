f17e4f8e8aca3812132349010f142b312efd648c
Delphit <m.mykhailyshyn@yandex.ru>
2017-07-25 14:23:34 +0300

    Update styles NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,32 +1,11 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import Item from './Item';
 import styles from './styles.scss';
 
 class NavBar extends Component {
-  constructor() {
-    super();
-    this.state = {
-      activeIndex: 0,
-    };
-    this.handleItemClick = this.handleItemClick.bind(this);
-  }
-  handleItemClick(index) {
-    this.state.activeIndex === index
-      ? this.setState({ activeIndex: null })
-      : this.setState({ activeIndex: index });
-  }
   render() {
-    const items = [
-      {
-        text: 'Add User',
-        icon: 'fa fa-users',
-      },
-      {
-        text: 'Settings',
-        icon: 'fa fa-cog',
-      },
-    ];
     return (
       <div className={styles.navbar}>
         <div className={styles.navbar__title}>
@@ -34,33 +13,15 @@ class NavBar extends Component {
           <span className={styles.navbar__title_text}>Project</span>
         </div>
         <ul className={styles.navbar__list}>
-          {items.map((item, index) => {
-            const className = this.state.activeIndex === index ? 'open' : null;
-            return (
-              <li
-                className={`${styles.navbar__item} ${className}`}
-                key={index}
-                onClick={() => this.handleItemClick(index)}
-              >
-                <i className={`${styles.navbar__item_icon} ${item.icon}`} />
-                {item.text}
-                <i
-                  className={`${styles.navbar__item_toggle} fa fa-chevron-down`}
-                />
-                <ul className={`${className ? 'submenu-open' : null} submenu`}>
-                  <li>
-                    <a href="#">Javascript</a>
-                  </li>
-                  <li>
-                    <a href="#">React</a>
-                  </li>
-                  <li>
-                    <a href="#">Angular.js</a>
-                  </li>
-                </ul>
-              </li>
-            );
-          })}
+          <Item text="Add User" icon="users">
+            <ul>
+              <li>as</li>
+              <li>as</li>
+              <li>as</li>
+              <li>as</li>
+            </ul>
+          </Item>
+          <Item text="Settings" icon="cog" link="/settings" />
         </ul>
       </div>
     );
