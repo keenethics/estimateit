0d83b6ab48589c1d49fe4112758f6005f3cf7196
kuksik <nazarkukarkin@gmail.co>
2017-08-02 13:32:48 +0300

    removed sidebar

--- src/routes/estimateGet/index.js
+++ src/routes/estimateGet/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Wrapper from './wrapper';
 import Layout from '../../components/Layout';
-import NavBar from '../../components/NavBar';
 import Header from '../../components/Header';
 import Main from '../../components/Main';
 
@@ -16,7 +15,6 @@ export default {
         component: (
           <Wrapper id={id}>
             <Layout>
-              <NavBar />
               <Header />
               <Main estimateId={id} />
             </Layout>
