18308df6f2654490f2a793faa8f5159db5c2be1e
tatomyr <andrew.tatomyr@gmail.com>
2017-08-29 11:51:58 +0300

    highlighted project name with red color

--- src/components/Dashboard/SingleEstimate.js
+++ src/components/Dashboard/SingleEstimate.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
+import {
+  Card,
+  CardBlock,
+  CardHeader,
+} from 'reactstrap';
 
 import * as styles from './styles.scss';
 
@@ -9,11 +14,12 @@ const SingleEstimate = ({ estimate }) => (
     href={`/estimate/${estimate._id}`}
     className={`${styles.singleEstimate} col-xs-12 col-sm-12 col-md-6 col-lg-4`}
   >
-    <div className={styles.card_pf}>
-      <div className={styles.card_pf__body}>
-        <div className={styles.card_pf__title}>
-          {estimate.projectName}
-        </div>
+
+    <Card>
+      <CardHeader className={styles.singleEstimate__header}>
+        {estimate.projectName}
+      </CardHeader>
+      <CardBlock>
         <div className={styles.singleEstimate__text}>
           Client name: {estimate.clientName}
         </div>
@@ -23,8 +29,8 @@ const SingleEstimate = ({ estimate }) => (
         <div className={styles.singleEstimate__text}>
           Date: {moment(estimate.date).format('MMM Do YY')}
         </div>
-      </div>
-    </div>
+      </CardBlock>
+    </Card>
   </a>
 );
 
--- src/components/Disclaimer/index.js
+++ src/components/Disclaimer/index.js
@@ -13,7 +13,7 @@ import {
 
 export default () => (
   <Card>
-    <CardHeader>Disclaimer / Notice / ?</CardHeader>
+    <CardHeader>Disclaimer</CardHeader>
     <CardBlock>
       <p>Current estimate document is a subject to change due to the following:</p>
       <p>Every additional request that wasn't initially within the estimated scope as well as improvements will be considered as an additional work load. The timeline will be extended accordingly to a cost increasement.</p>
