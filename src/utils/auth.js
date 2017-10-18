223e6fbc4b0adf73e879e4e1151bf25f6285e154
michaelost <mykhailo.ostroviy@keenethics.com>
2017-10-18 16:01:17 +0300

    refactor

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -22,84 +22,54 @@ passport.use(
     (req, accessToken, refreshToken, profile, done) => {
       const { displayName, id, emails } = profile;
       const email = (emails[0].value || '').toLowerCase();
+      const google = {
+        id: profile.id, 
+        token: accessToken,
+        refreshToken,
+      }
+      const userObj = {
+        google,
+        name: displayName,
+        email: email,
+        status: ACTIVE,
+      }
+      const message = 'Successfully Logged In';
       process.nextTick(async () => {
-        if (!req.user) {
-          try {
+        try {
+          if (!req.user) {
             const userWithEmail = await User.findOne({ email });
             if (userWithEmail && userWithEmail.status === PENDING) {
-              const u = userWithEmail;
-              u.google.id = id;
-              u.name = displayName;
-              u.google.token = accessToken;
-              u.google.refreshToken = refreshToken;
-              u.email = email;
-              u.status = ACTIVE;
+              const u = Object.assign({}, userObj, userWithEmail);
               await u.save();
               return done(null, {
                 success: true,
-                message: 'Successfully Logged In',
+                message,
                 user: u,
               });                 
-
-
-            } else {
-
-              const user = await User.findOne({ 'google.id': profile.id });
-              let returnUser = user;
-              if (user) {
-                  const u = user;
-                  u.google.id = profile.id;
-                  u.google.token = accessToken;
-                  u.google.refreshToken = refreshToken;
-                  returnUser = await u.save(); 
-              } else {
-                  const newUser = new User();
-                  newUser.status = ACTIVE;
-                  newUser.google.id = profile.id;
-                  newUser.google.token = accessToken;
-                  newUser.google.refreshToken = refreshToken;
-                  newUser.name = profile.displayName;
-                  newUser.email = email;
-                  returnUser = await newUser.save();
-
-              }
+            }
+            const user = await User.findOne({ 'google.id': profile.id });
+            if (user) {
+              user.google = google;
+              await user.save(); 
               return done(null, {
                 success: true,
-                message: 'Successfully Logged In',
-                user: returnUser,
+                message,
+                user,
               });              
-
-              
             }
-          } catch (error) {
-              return done(null, false, {
-                success: false,
-                message: error,
-              });     
           }
-        } else {
-          try {
-            const user = req.user;
-            user.status = ACTIVE;
-            user.google.token = profile.id;
-            user.google.token = accessToken;
-            user.google.refreshToken = refreshToken;
-            user.name = profile.displayName;
-            user.email = email;
-            await user.save();
-            return done(null, {
-              success: true,
-              message: 'Successfully Logged In',
-              user,
-            });
-
-          } catch (err) {
-              return done(null, false, {
-                success: false,
-                message: error,
-              });        
-          }
-
+          const user = Object.assign({}, userObj, req.user);
+          await user.save();
+          return done(null, {
+            success: true,
+            message,
+            user,
+          });
+        } catch (error) {
+           return done(null, false, {
+            success: false,
+            message: error,
+          });           
         }
       });
     },
