2d2ca71d9004bda5c67dcf9b3a1be122f7d1d341
michaelost <mykhailo.ostroviy@keenethics.com>
2017-10-18 13:03:57 +0300

    refactor auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -20,112 +20,91 @@ passport.use(
       passReqToCallback: true,
     },
     (req, accessToken, refreshToken, profile, done) => {
+      const { displayName, id, emails } = profile;
+      const email = (emails[0].value || '').toLowerCase();
       process.nextTick(async () => {
         if (!req.user) {
-          const email = (profile.emails[0].value || '').toLowerCase();
           try {
             const userWithEmail = await User.findOne({ email });
-
             if (userWithEmail && userWithEmail.status === PENDING) {
-              User.findOne({ email }, (err, user) => {
-                if (err) {
-                  return done(err);
-                }
-                if (user) {
+              const u = userWithEmail;
+              u.google.id = id;
+              u.name = displayName;
+              u.google.token = accessToken;
+              u.google.refreshToken = refreshToken;
+              u.email = email;
+              u.status = ACTIVE;
+              await u.save();
+              return done(null, {
+                success: true,
+                message: 'Successfully Logged In',
+                user: u,
+              });                 
+
+
+            } else {
+
+              const user = await User.findOne({ 'google.id': profile.id });
+              let returnUser = user;
+              if (user) {
+                if (!user.google.token) {
                   const u = user;
                   u.google.id = profile.id;
-                  u.name = profile.displayName;
                   u.google.token = accessToken;
                   u.google.refreshToken = refreshToken;
+                  u.name = profile.displayName;
                   u.status = ACTIVE;
-                  u.save((error) => {
-                    if (error) {
-                      return done(null, false, {
-                        success: false,
-                        message: error,
-                      });
-                    }
-                    return done(null, {
-                      success: true,
-                      message: 'Successfully Logged In',
-                      user,
-                    });
-                  });
+                  u.email = email;
+                  returnUser = await u.save(); 
                 }
-              });
-            } else {
-              User.findOne(
-                {
-                  'google.id': profile.id,
-                },
-                (err, user) => {
-                  if (err) {
-                    return done(err);
-                  }
-                  if (user) {
-                    const u = user;
-                    u.google.id = profile.id;
-                    u.google.token = accessToken;
-                    u.google.refreshToken = refreshToken;
-                    u.name = profile.displayName;
-                    u.status = ACTIVE;
-                    u.save((error) => {
-                      if (error) {
-                        return done(null, false, {
-                          success: false,
-                          message: error,
-                        });
-                      }
-                      return done(null, {
-                        success: true,
-                        message: 'Successfully Logged In',
-                        user,
-                      });
-                    });
-                  }
-
+              } else {
                   const newUser = new User();
                   newUser.status = ACTIVE;
                   newUser.google.id = profile.id;
                   newUser.google.token = accessToken;
                   newUser.google.refreshToken = refreshToken;
                   newUser.name = profile.displayName;
-                  newUser.email = (profile.emails[0].value || '').toLowerCase();
-                  newUser.save((error) => {
-                    if (error) {
-                      return done(error);
-                    }
-                    return done(null, {
-                      success: true,
-                      message: 'Successfully Logged In',
-                      user: newUser,
-                    });
-                  });
-                },
-              );
+                  newUser.email = email;
+                  returnUser = await newUser.save();
+
+              }
+              return done(null, {
+                success: true,
+                message: 'Successfully Logged In',
+                user: returnUser,
+              });              
+
+              
             }
           } catch (error) {
-            return done(error);
+              return done(null, false, {
+                success: false,
+                message: error,
+              });     
           }
         } else {
-          const user = req.user;
-          user.status = ACTIVE;
-          user.google.token = profile.id;
-          user.google.token = accessToken;
-          user.google.refreshToken = refreshToken;
-          user.name = profile.displayName;
-          user.email = (profile.emails[0].value || '').toLowerCase();
-          user.save((err) => {
-            if (err) {
-              return done(err);
-            }
-
+          try {
+            const user = req.user;
+            user.status = ACTIVE;
+            user.google.token = profile.id;
+            user.google.token = accessToken;
+            user.google.refreshToken = refreshToken;
+            user.name = profile.displayName;
+            user.email = email;
+            await user.save();
             return done(null, {
               success: true,
               message: 'Successfully Logged In',
               user,
             });
-          });
+
+          } catch (err) {
+              return done(null, false, {
+                success: false,
+                message: error,
+              });        
+          }
+
         }
       });
     },
